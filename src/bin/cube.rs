45a653435786f58afaf67338a81872eeb56164d3
cynic64 <f3bl@gmx.com>
2019-10-08 20:59:21 +0200

    updated cube

--- src/bin/cube.rs
+++ src/bin/cube.rs
@@ -6,7 +6,7 @@ use re::App;
 pub fn main() {
     let mut app = App::new();
     let mut world_com = app.get_world_com();
-    let spec = ObjectSpecBuilder::default().build(app.get_device());
+    let spec = ObjectSpecBuilder::default().build();
     world_com.add_object_from_spec("cube", spec);
 
     while !app.done {
